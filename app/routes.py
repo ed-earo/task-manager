79ea9c7e36cd478e004ce2ea8927fe21237584f2
Eduardo <eduardoaroliveira@protonmail.com>
2026-07-16 16:25:04 -0300

    Build initial webpage structure

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 main = Blueprint("main", __name__)
@@ -6,4 +6,4 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return "Welcome to Task Manager!"
+    return render_template("index.html")
